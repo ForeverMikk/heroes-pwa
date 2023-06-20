fcbadef5fdc123d4748a861c418d91d5efff929e
unknown <michaelmolina679@gmail.com>
2023-06-19 18:56:47 -0600

    archivos de barril para todos mis archivos

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+
 import { Navbar } from "../ui"
+import { DcPage, MarvelPage } from "../heroes"
+import { LoginPage } from "../auth"
 
 export const AppRouter = () => {
   return (
@@ -10,7 +10,7 @@ export const AppRouter = () => {
     <Navbar />
 
     <Routes>
-        <Route path="marvel" element={<MarvelPage/>} />
+        <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DcPage />} />
         
         <Route path="login" element={<LoginPage />} />
