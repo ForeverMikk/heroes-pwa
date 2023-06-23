16af7203b942ab02e1a18f7e6d5a6cb46056549a
unknown <michaelmolina679@gmail.com>
2023-06-22 18:59:19 -0600

    Mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,13 +12,16 @@ export const SearchPage = () => {
   const {q = ''} = queryString.parse(location.search); //con query string separas los queryParameteers de manera mas facil
   const heroes = getHeroesByName(q);
 
+  const showSearch = (q.length === 0); //Si no hay nada en el query es true
+  const showError = (q.length > 0) && heroes.length === 0; // Si hay algo en el query pero no regreso ningun heroe es true
+
   const { onInputChange, searchText } = useForm({
     searchText: q
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if(searchText.trim().length <= 1) return;
+    // if(searchText.trim().length <= 1) return;
 
     navigate(`?q=${searchText}`);
   }
@@ -54,12 +57,25 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {/* Hotra Forma de mostrar el Texto */}
+          {/* {
+            (q === '')
+            ? <div className="alert alert-primary">Search a Hero</div>
+            : (heroes.length === 0) && <div className="alert alert-danger">No resulrs <b>{ q }</b></div>
+          } */}
+
+          <div 
+            className="alert alert-primary animate__animated animate__fadeIn" 
+            style={{display: showSearch ? '' : 'none'}}
+          >
             Search a Hero
           </div>
 
-          <div className="alert alert-danger">
-            No resulrs <b>{ q }</b>
+          <div 
+            className="alert alert-danger animate__animated animate__fadeInLeft" 
+            style={{display: showError ? '' : 'none'}}
+          >
+            not found <b>{q}</b>
           </div>
 
           {heroes.map(heroe => (
