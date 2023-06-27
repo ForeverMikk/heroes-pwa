48194ffb5b3599a812997955b755d00cd237e34c
unknown <michaelmolina679@gmail.com>
2023-06-27 13:36:58 -0600

    Corrigiendo errores en el Router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,16 +2,12 @@ import { Route, Routes } from "react-router-dom"
 
 import { HeroesRoutes } from "../heroes"
 import { LoginPage } from "../auth"
-import { Navbar } from "../ui"
 import { PrivatreRoute } from "./PrivatreRoute"
 import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
   return (
-
-    
-    <>     
-    <Navbar />
+    <>
       <Routes>
           
           <Route path="/login/*" element={
